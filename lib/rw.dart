478053ec36f762495039c07af5a4bc294fa159ae
ruwiss <bihinmehen@gmail.com>
2024-05-14 18:39:18 +0300

    icon-pack

--- lib/rw.dart
+++ lib/rw.dart
@@ -1,24 +1,25 @@
 library rw;
 
 // Theme
-export 'src/app/theme/base_theme.dart';
 export 'src/app/theme/light_theme.dart';
+export 'src/app/theme/base_theme.dart';
 export 'src/app/theme/dark_theme.dart';
 
 // Core
-export 'src/core/base/base_viewmodel.dart';
-export 'src/core/clients/network_client.dart';
 export 'src/core/extensions/context_extensions.dart';
+export 'src/core/clients/network_client.dart';
+export 'src/core/base/base_viewmodel.dart';
 
 // Utils
-export 'src/core/utils/device_info.dart';
 export 'src/core/utils/package_info.dart';
+export 'src/core/utils/device_info.dart';
 
 // Main
 export 'src/bootstrap.dart';
 export 'src/locator.dart';
 
 // Dependencies
-export 'package:dio/dio.dart';
+export 'package:fluentui_system_icons/fluentui_system_icons.dart';
 export 'package:fluid_dialog/fluid_dialog.dart';
 export 'package:animations/animations.dart';
+export 'package:dio/dio.dart';
